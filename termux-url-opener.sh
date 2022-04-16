d0c94d8e171eaa7fa8d3a44ab1f65b1ef3bf4425
termuxbyte <50546720+termuxbyte@users.noreply.github.com>
2022-04-16 08:04:17 -0500

    Update termux-url-opener.sh

--- termux-url-opener.sh
+++ termux-url-opener.sh
@@ -1,27 +1,41 @@
-#!/bin/bash
-# Copyright 2017 Gabi Tiplea
-
-echo "Copyright 2017 Gabi Tiplea"
-echo "For audio only press 1"
-echo "For video 360p press 2"
-echo "For video 480p press 3"
-echo "For video 720p press 4"
-echo "For video 1080p press 5"
-read option
+#!/data/data/com.termux/files/usr/bin/bash
+ 
+echo "Termux Byte \n"
+ 
+echo "Tu video se guardara en tu memoria interna-Carpeta Youtobe \n"
+ 
+echo "SELECIONE EL FORMATO DEL VIDEO \n"
+echo "Para Audio mp3 presione 1"
+echo "video 360px presione 2"
+echo "video 480px presione 3"
+echo "video 720px presione 4"
+echo "video 1080px presione 5"
+ 
 command='-no-mtime -o /data/data/com.termux/files/home/storage/shared/Youtube/%(title)s.%(ext)s -f'
-if [ "$option" -eq "1" ]; then
-    echo "$command 140" > $HOME/.config/youtube-dl/config
+read option
+ 
+if [ "$option" -eq "1" ]
+then
+    echo "$command 140" > ~/.config/youtube-dl/config
     youtube-dl $1
-elif [ "$option" -eq "2" ]; then
-    echo "$command \"best[height<=360]\"" > $HOME/.config/youtube-dl/config
+ 
+elif [ "$option" -eq "2" ]
+then
+    echo "$command \"best[height<=360]\"" > ~/.config/youtube-dl/config
     youtube-dl $1
-elif [ "$option" -eq "3" ]; then
-    echo "$command \"best[height<=480]\"" > $HOME/.config/youtube-dl/config
+ 
+elif [ "$option" -eq "3" ]
+then
+    echo "$command \"best[height<=480]\"" > ~/.config/youtube-dl/config
     youtube-dl $1
-elif [ "$option" -eq "4" ]; then
-    echo "$command \"best[height<=720]\"" > $HOME/.config/youtube-dl/config
+ 
+elif [ "$option" -eq "4" ]
+then
+    echo "$command \"best[height<=720]\"" > ~/.config/youtube-dl/config
     youtube-dl $1
-elif [ "$option" -eq "5" ]; then
-    echo "$command \"best[height<=1080]\"" > $HOME/.config/youtube-dl/config
+ 
+elif [ "$option" -eq "5" ]
+then
+    echo "$command \"best[height<=1080]\"" > ~/.config/youtube-dl/config
     youtube-dl $1
 fi
